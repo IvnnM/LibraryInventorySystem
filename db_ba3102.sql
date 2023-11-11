497498f0a73c84d12371fbd1ccf2f09ca7cfcc81
Ivan Medrano <143078241+IvnnM@users.noreply.github.com>
2023-11-11 17:36:19 +0800

    Add files via upload

--- db_ba3102.sql
+++ db_ba3102.sql
@@ -13,14 +13,13 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `libraryinventorysystem`
+-- Database: `db_ba3102`
 --
 
 -- --------------------------------------------------------
@@ -30,22 +29,31 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `books` (
-  `bookID` int(11) NOT NULL,
+  `bookID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `ISBN` varchar(50) NOT NULL,
   `description` text DEFAULT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `publication_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`bookID`, `title`, `description`, `quantity`) VALUES
-(1, 'Book 1', 'Description of Book 1', 61),
-(2, 'Book 2', 'Description of Book 2', 10),
-(3, 'Book 3', 'Description of Book 3', 5),
-(4, 'Book 4', 'Description of Book 4', 20),
-(5, 'Book 5', 'Description of Book 5', 0);
+INSERT INTO `books` (`title`, `author`, `genre`, `ISBN`, `description`, `quantity`, `publication_date`) VALUES
+('Book 1', 'Author 1', 'Genre 1', 'ISBN123456', 'Description of Book 1', 10, '2022-01-15'),
+('Book 2', 'Author 2', 'Genre 2', 'ISBN234567', 'Description of Book 2', 15, '2022-02-20'),
+('Book 3', 'Author 3', 'Genre 3', 'ISBN345678', 'Description of Book 3', 8, '2022-03-25'),
+('Book 4', 'Author 1', 'Genre 4', 'ISBN456789', 'Description of Book 4', 20, '2022-04-10'),
+('Book 5', 'Author 1', 'Genre 1', 'ISBN101112', 'Description of Book 5', 20, '2022-05-05'),
+('Book 6', 'Author 1', 'Genre 1', 'ISBN789012', 'Description of Book 6', 15, '2022-06-15'),
+('Book 7', 'Author 2', 'Genre 2', 'ISBN890123', 'Description of Book 7', 12, '2022-07-20'),
+('Book 8', 'Author 2', 'Genre 2', 'ISBN901234', 'Description of Book 8', 18, '2022-08-25'),
+('Book 9', 'Author 2', 'Genre 2', 'ISBN012345', 'Description of Book 9', 25, '2022-09-10'),
+('Book 10', 'Author 3', 'Genre 3', 'ISBN123456', 'Description of Book 10', 10, '2022-10-05');
 
 -- --------------------------------------------------------
 
@@ -54,7 +62,7 @@ INSERT INTO `books` (`bookID`, `title`, `description`, `quantity`) VALUES
 --
 
 CREATE TABLE `book_transactions` (
-  `transactionID` int(11) NOT NULL,
+  `transactionID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `userID` int(11) NOT NULL,
   `bookID` int(11) NOT NULL,
   `inQuantity` int(11) NOT NULL,
@@ -66,20 +74,18 @@ CREATE TABLE `book_transactions` (
 -- Dumping data for table `book_transactions`
 --
 
-INSERT INTO `book_transactions` (`transactionID`, `userID`, `bookID`, `inQuantity`, `outQuantity`, `date`) VALUES
-(23, 3, 1, 2, 0, '2023-10-22 09:20:17'),
-(24, 3, 2, 0, 1, '2023-10-22 09:20:23'),
-(25, 3, 3, 1, 0, '2023-10-22 09:20:29'),
-(26, 3, 4, 0, 3, '2023-10-23 04:20:00'),
-(27, 2, 5, 2, 0, '2023-10-23 06:05:00'),
-(28, 3, 1, 4, 0, '2023-10-23 03:14:51'),
-(29, 2, 1, 0, 5, '2023-10-23 03:20:58'),
-(30, 3, 2, 0, 5, '2023-10-23 04:53:56'),
-(31, 3, 1, 1, 0, '2023-10-23 05:08:34'),
-(32, 3, 1, 0, 1, '2023-10-23 05:08:45'),
-(33, 3, 1, 1, 0, '2023-10-23 05:10:03'),
-(34, 3, 1, 0, 1, '2023-10-23 05:12:50'),
-(35, 3, 1, 1, 0, '2023-10-23 05:18:50');
+-- Inserting data into `book_transactions`
+INSERT INTO `book_transactions` (`userID`, `bookID`, `inQuantity`, `outQuantity`, `date`) VALUES
+(2, 6, 2, 0, '2023-11-02 09:20:17'),
+(3, 7, 0, 1, '2023-11-02 09:20:23'),
+(2, 8, 1, 0, '2023-11-02 09:20:29'),
+(3, 9, 0, 3, '2023-11-03 04:20:00'),
+(3, 10, 2, 0, '2023-11-03 06:05:00'),
+(3, 11, 4, 0, '2023-11-03 03:14:51'),
+(2, 12, 0, 5, '2023-11-03 03:20:58'),
+(3, 13, 0, 5, '2023-11-03 04:53:56'),
+(3, 14, 1, 0, '2023-11-03 05:08:34'),
+(3, 15, 0, 1, '2023-11-03 05:08:45');
 
 -- --------------------------------------------------------
 
@@ -88,7 +94,7 @@ INSERT INTO `book_transactions` (`transactionID`, `userID`, `bookID`, `inQuantit
 --
 
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -99,69 +105,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `fullname`, `email`, `password`, `role`) VALUES
-(1, 'Admin User', 'admin@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Admin'),
-(2, 'Client User', 'client@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Client'),
-(3, 'Librarian User', 'librarian@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Librarian');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`bookID`);
-
---
--- Indexes for table `book_transactions`
---
-ALTER TABLE `book_transactions`
-  ADD PRIMARY KEY (`transactionID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `bookID` (`bookID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `book_transactions`
---
-ALTER TABLE `book_transactions`
-  MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `book_transactions`
---
-ALTER TABLE `book_transactions`
-  ADD CONSTRAINT `book_transactions_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
-  ADD CONSTRAINT `book_transactions_ibfk_2` FOREIGN KEY (`bookID`) REFERENCES `books` (`bookID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `users` (`fullname`, `email`, `password`, `role`) VALUES
+('Admin User', 'admin@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Admin'),
+('Client User', 'client@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Client'),
+('Librarian User', 'librarian@g', '$2y$10$7WBXttrmFnqyZgP/w3hQCuj1UFGktv.zrkE7vVALTOhiOQa.LSSFq', 'Librarian');
